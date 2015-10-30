150d0d00785a47aa0c1c1b5b1c96ba7143311812
Kyle Spraggs <theman@spiffyjr.me>
2015-10-30 10:45:29 -0500

    Sample drop-down

--- src/app/component/layout/controller.js
+++ src/app/component/layout/controller.js
@@ -3,8 +3,9 @@ var app = angular.module('app');
 app.controller('layoutCtrl', [
     '$scope',
     '$state',
+    'gettextCatalog',
 
-    function ($scope, $state) {
+    function ($scope, $state, gettextCatalog) {
         $scope.searchForPlayer = function(name) {
             if (!name || name.trim().length == 0) {
                 return;
@@ -12,5 +13,19 @@ app.controller('layoutCtrl', [
 
             $state.go('app.search', { name: name });
         };
+
+        $scope.languages = {
+            de: 'Deutsch',
+            en: 'English',
+            es: 'Español',
+            fr: 'Français',
+            it: 'Italiano',
+            'pt-br': 'Português (Brasil)',
+            ja: '日本語'
+        };
+
+        $scope.currentLanguage = function() {
+            return gettextCatalog.getCurrentLanguage();
+        }
     }
 ]);
